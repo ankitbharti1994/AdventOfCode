27fbcbe4e908388c556d6e8608430f8a093195b7
ankit bharti <ankitbharti1994@gmail.com>
2019-11-02 13:16:13 +0530

    completed first part

--- 2018/Swift/Day02.playground/Contents.swift
+++ 2018/Swift/Day02.playground/Contents.swift
@@ -1,3 +1,26 @@
 import Foundation
 
-var str = "Hello, playground"
+"abcdef".duplicateCharacterCount
+"bababc".duplicateCharacterCount
+"abbcde".duplicateCharacterCount
+"abcccd".duplicateCharacterCount
+"aabcdd".duplicateCharacterCount
+"abcdee".duplicateCharacterCount
+"ababab".duplicateCharacterCount
+
+["abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab"].checksum
+readInput().checksum
+
+let test = Day02Test()
+
+test.testInput1()
+test.testInput2()
+test.testInput3()
+test.testInput4()
+test.testInput5()
+test.testInput6()
+test.testInput7()
+test.testInput8()
+
+test.testInputFile1()
+
